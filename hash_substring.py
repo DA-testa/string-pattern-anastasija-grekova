35a3d50c8ba191dd52fb85970fdd490388289a0e
anastasija-grekova <125025406+anastasija-grekova@users.noreply.github.com>
2023-04-15 18:27:37 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,20 +1,22 @@
 import os
 
 def read_input():
-    # this function needs to acquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
     check = input().rstrip()
-
 
     if check == "F":
         path = os.getcwd() + '/tests'
         os.chdir(path)
         file_name = input()
 
+        #print(file_name)
+        #quit()
+
         if 'a' in file_name:
             print("error")
             quit()        
         file_path = f"{path}/{file_name}" 
+        #print(file_path)
+        #quit()
         
         with open(file_path, "r", encoding="utf-8-sig") as f:
             pattern = f.readline().rstrip()
@@ -32,37 +34,28 @@ def read_input():
 
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
-
 def get_occurrences(pattern, text):
-    # this function should find the occurrences using Rabin Karp algorithm
-    occurrences = []
-    p_len = len(pattern)
-    t_len = len(text)
+    count = []
+    patternLength = len(pattern)
+    #textLength = len(text)
 
-    # calculate the hash value of the pattern
-    p_hash = sum(ord(pattern[i]) * pow(10, p_len - i - 1) for i in range(p_len)) 
+    patternHashVal = sum(ord(pattern[i]) * pow(10, patternLength - i - 1) for i in range(patternLength)) 
 
-    # calculate the hash value of the first window in the text
-    t_hash = sum(ord(text[i]) * pow(10, p_len - i - 1) for i in range(p_len))
+    textHashVal = sum(ord(text[i]) * pow(10, patternLength - i - 1) for i in range(patternLength))
 
-    # check if pattern matches with the first window
-    if p_hash == t_hash and pattern == text[:p_len]:
-        occurrences.append(0)
+    if patternHashVal == textHashVal and pattern == text[:patternLength]:
+        count.append(0)
 
-    # calculate the hash value of each subsequent window and check if it matches with the pattern
-    for i in range(1, t_len - p_len + 1):
-        t_hash = t_hash - ord(text[i - 1]) * pow(10, p_len - 1)
-        t_hash = t_hash * 10 + ord(text[i + p_len - 1])
-        if p_hash == t_hash and pattern == text[i:i+p_len]:
-            occurrences.append(i)
+    for i in range(1, len(text) - patternLength + 1):
+        textHashVal = textHashVal - ord(text[i - 1]) * pow(10, patternLength - 1)
+        textHashVal = textHashVal * 10 + ord(text[i + patternLength - 1])
+        if patternHashVal == textHashVal and pattern == text[i:i+patternLength]:
+            count.append(i)
 
-    return occurrences
+    return count
 
 
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
